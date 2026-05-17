b08bb621f9ee6c15437389c912ff0bef675c4bcd
Kautuk Adarsh <kautukadarsh1@gmail.com>
2026-05-17 12:40:04 +0530

    fix redis TLS for production

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,11 +1,14 @@
 const { Redis } = require('ioredis');
 
+const redisOptions = {
+  maxRetriesPerRequest: null,
+  ...(process.env.REDIS_URL?.startsWith('rediss://') && { tls: {} })
+};
+
 const redis = new Redis(process.env.REDIS_URL);
 redis.on('error', (err) => console.error('Redis error:', err));
 
-const bullMQRedis = new Redis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: null
-});
+const bullMQRedis = new Redis(process.env.REDIS_URL, redisOptions);
 bullMQRedis.on('error', (err) => console.error('BullMQ Redis error:', err));
 
 module.exports = { redis, bullMQRedis };
